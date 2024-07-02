69479fbfd8e9f89fcffad03c04d40df269c90b69
Abdelalim <162515953+AbdelalimB1729@users.noreply.github.com>
2024-07-02 00:01:09 -0400

    partials

---
+++ projet php/partials/header.php
@@ -0,0 +1,238 @@
+<header class="speedyui speedyui-header">
+    <div class="top-bar py-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-auto col-lg-4">
+                    <span class="d-none d-lg-flex align-items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                        </svg>
+                        <a class="nav-link text-uppercase" href="#">1 855 588 2436 </a>
+                    </span>
+                </div>
+                <div class="col-12 col-md-auto mx-auto">
+                    <a class="nav-link text-uppercase text-center" href="#">Discover what makes BHFO unique!</a>
+                </div>
+                <div class="col-auto col-lg-4">
+                    <ul class="d-none d-lg-flex align-items-center justify-content-end gap-2 gap-lg-3 list-unstyled mb-0">
+                        <li>
+                            <a class="nav-link text-uppercase" href="#">About us</a>
+                        </li>
+                        <li>
+                            <a class="nav-link text-uppercase" href="#">Gift cards</a>
+                        </li>
+                        <li>
+                            <a class="nav-link text-uppercase" href="#">Shipping</a>
+                        </li>
+                        <li>
+                            <a class="nav-link text-uppercase" href="#">Returns</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav id="navb" class="bg-light navbar navbar-expand-lg py-2 py-md-0">
+        <div class="container py-2 py-lg-0">
+            <button class="navbar-toggler border-0 shadow-none px-0 me-2" type="button"  data-bs-toggle="offcanvas" href="#offcanvas" role="button" aria-controls="offcanvas">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+                <img class="img-fluid" src="./images/Screenshot_2024-06-27_001249-removebg-preview.png" width="120px" alt="" />
+            </a>
+            <div class="social-wrapper order-1 order-lg-2 d-flex align-items-center gap-3 ms-auto">
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                    </svg>
+                </span>
+                <span>
+                    <form class="d-flex align-items-center" role="search">
+                        <input id="form-control" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                    </form>
+                </span>
+                <span>
+                    <svg width="16" height="16" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M9 8.31836C11.0711 8.31836 12.75 6.63943 12.75 4.56836C12.75 2.49729 11.0711 0.818359 9 0.818359C6.92893 0.818359 5.25 2.49729 5.25 4.56836C5.25 6.63943 6.92893 8.31836 9 8.31836Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M0.818359 17.1819C0.818359 15.2531 1.68037 13.4032 3.21476 12.0393C4.74915 10.6754 6.83022 9.90918 9.00018 9.90918C11.1701 9.90918 13.2512 10.6754 14.7856 12.0393C16.32 13.4032 17.182 15.2531 17.182 17.1819" stroke="currentColor" stroke-linejoin="round"></path>
+                    </svg>
+                </span>
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-handbag" viewBox="0 0 16 16">
+                        <path d="M8 1a2 2 0 0 1 2 2v2H6V3a2 2 0 0 1 2-2zm3 4V3a3 3 0 1 0-6 0v2H3.36a1.5 1.5 0 0 0-1.483 1.277L.85 13.13A2.5 2.5 0 0 0 3.322 16h9.355a2.5 2.5 0 0 0 2.473-2.87l-1.028-6.853A1.5 1.5 0 0 0 12.64 5H11zm-1 1v1.5a.5.5 0 0 0 1 0V6h1.639a.5.5 0 0 1 .494.426l1.028 6.851A1.5 1.5 0 0 1 12.678 15H3.322a1.5 1.5 0 0 1-1.483-1.723l1.028-6.851A.5.5 0 0 1 3.36 6H5v1.5a.5.5 0 1 0 1 0V6h4z"/>
+                    </svg>
+                </span>
+            </div>
+            <div class="navbar-collapse order-2 order-lg-1 collapse py-2 py-lg-0" id="navbarNavDropdown">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link px-3 py-2 py-lg-4" href="#">
+                            Living room
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3 py-2 py-lg-4" href="#">
+                            Dining room
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3 py-2 py-lg-4" href="#">
+                            Bedroom
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3 py-2 py-lg-4" href="#">
+                            Flooring
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown dropdown-megamenu position-static">
+                        <a class="nav-link dropdown-toggle d-flex align-items-center justify-content-start justify-content-lg-center gap-2 px-3 py-2 py-lg-4" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Accessories
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                    <path stroke="currentColor" stroke-width="1.3" fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </span>
+                        </a>
+                        <div class="w-100 dropdown-menu border-0 start-50 translate-middle-x shadow-none py-4 mt-0">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col border-end">
+                                        <h4 class="small text-secondary text-uppercase opacity-50 px-3 mb-3">Furniture</h4>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item mb-1" href="#">Tables</a></li>
+                                            <li><a class="dropdown-item mb-1" href="#">Drawers</a></li>
+                                            <li><a class="dropdown-item mb-1" href="#">Mirrors</a></li>
+                                            <li><a class="dropdown-item mb-1" href="#">Cabinets</a></li>
+                                            <li><a class="dropdown-item mb-1" href="#">Wardrobes</a></li>
+                                            <li><a class="dropdown-item mb-1" href="#">Headboards</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col border-end">
+                                        <h4 class="small text-secondary text-uppercase opacity-50 px-3 mb-3">Dining</h4>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item mb-1" href="#">Bookcases</a></li>
+                                            <li><a class="dropdown-item mb-1" href="#">Bar Stools</a></li>
+                                            <li><a class="dropdown-item mb-1" href="#">Sideboards</a></li>
+                                            <li><a class="dropdown-item mb-1" href="#">Dining Chairs</a></li>
+                                            <li><a class="dropdown-item mb-1" href="#">Shelving Units</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col">
+                                        <h4 class="small text-secondary text-uppercase opacity-50 px-3 mb-3">Flooring</h4>
+                                        <ul class="list-unstyled">
+                                            <li><a class="dropdown-item mb-1" href="#">Rugs</a></li>
+                                            <li><a class="dropdown-item mb-1" href="#">Carpets</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-4">
+                                        <a href="#"><img class="img-fluid" src="https://speedyui-static.s3.amazonaws.com/header-ecom/store-ads.jpg" alt=""></a>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3 py-2 py-lg-4" href="#">
+                            Brands
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
+        <div class="offcanvas-body position-relative p-0">
+            <div class="outer-listings bg-white text-dark w-100 h-100 pb-3">
+                <div class="submenu-header bg-light text-dark d-flex align-items-center justify-content-between gap-3 px-4 py-3 mb-3">
+                    <span class="menu-heading text-uppercase small mb-0">Menu</span>
+                    <button type="button" class="btn-close border-0 shadow-none ms-auto" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <ul class="offcanvas-listing list-unstyled">
+                    <li>
+                        <a class="nav-link px-3 py-3" href="#">
+                            Living room
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link px-3 py-3" href="#">
+                            Dining room
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link px-3 py-3" href="#">
+                            Bedroom
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link px-3 py-3" href="#">
+                            Flooring
+                        </a>
+                    </li>
+                    <li>
+                        <a id="submenu-handler" class="nav-link d-flex align-items-center justify-content-between px-3 py-3" href="#">
+                           Accessories
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </span>
+                        </a>
+                        <div class="submenu bg-white text-dark w-100 h-100 position-absolute top-0 pb-3 translate-x-full">
+                            <div class="submenu-header bg-light text-dark d-flex align-items-center justify-content-between gap-3 px-4 py-3 mb-4">
+                                <span id="submenu-back" class="d-inline-flex text-uppercase small align-items-center">
+                                    <svg class="me-2" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                                    </svg>Back
+                                </span>
+                                <button type="button" class="btn-close border-0 shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            </div>
+                            <ul class="list-unstyled ps-3">
+                                <li class="mb-4">
+                                    <h4 class="small text-secondary text-uppercase opacity-50 px-3 pb-3 mb-0">Furniture</h4>
+                                    <ul class="list-unstyled">
+                                        <li><a class="nav-link px-4 py-2" href="#">Tables</a></li>
+                                        <li><a class="nav-link px-4 py-2" href="#">Drawers</a></li>
+                                        <li><a class="nav-link px-4 py-2" href="#">Mirrors</a></li>
+                                        <li><a class="nav-link px-4 py-2" href="#">Cabinets</a></li>
+                                        <li><a class="nav-link px-4 py-2" href="#">Wardrobes</a></li>
+                                        <li><a class="nav-link px-4 py-2" href="#">Headboards</a></li>
+                                    </ul>
+                                </li>
+                                <li class="mb-4">
+                                    <h4 class="small text-secondary text-uppercase opacity-50 px-3 pb-3 mb-0">Dining</h4>
+                                    <ul class="list-unstyled">
+                                        <li><a class="nav-link px-4 py-2" href="#">Bookcases</a></li>
+                                        <li><a class="nav-link px-4 py-2" href="#">Bar Stools</a></li>
+                                        <li><a class="nav-link px-4 py-2" href="#">Sideboards</a></li>
+                                        <li><a class="nav-link px-4 py-2" href="#">Dining Chairs</a></li>
+                                        <li><a class="nav-link px-4 py-2" href="#">Shelving Units</a></li>
+                                    </ul>
+                                </li>
+                                <li class="mb-4">
+                                    <h4 class="small text-secondary text-uppercase opacity-50 px-3 pb-3 mb-0">Flooring</h4>
+                                    <ul class="list-unstyled">
+                                        <li><a class="nav-link px-4 py-2" href="#">Rugs</a></li>
+                                        <li><a class="nav-link px-4 py-2" href="#">Carpets</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="nav-link px-3 py-3" href="#">
+                            Brands
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ projet php/partials/main.php
@@ -0,0 +1,55 @@
+<main>
+  <div class="container">
+    <div class="d-flex justify-content-between align-items-center">
+        <div class="arrow" id="left-arrow">&lt;</div>
+        <div class="category-container" id="category-container">
+            <div class="category">
+                <img src="./images/a-yellow-and-white-background-with-a-logo-that-reads-summer--5099e464865014879c25609f6bdc5f77.avif" alt="Summer SALE">
+                <div class="category-title">Offres d'ete</div>
+            </div>
+            <div class="category">
+                <img src="./images/kulturskog-plant-stand-light-green__1185312_pe898352_s5.avif" alt="New">
+                <div class="category-title">Nouveautés</div>
+            </div>
+            <div class="category">
+              <img src="./images/furniture-fu001.avif" alt="New">
+              <div class="category-title">Tous les meubles</div>
+          </div>
+            <div class="category">
+                <img src="./images/home-improvement-hi001.webp" alt="All furniture">
+                <div class="category-title">All furniture</div>
+            </div>
+            <div class="category">
+                <img src="./images/kitchen-appliances-ka001.avif" alt="Summer SALE">
+                <div class="category-title">Summer SALE</div>
+            </div>
+            <div class="category">
+                <img src="./images/smart-home-hs001.webp" alt="All furniture">
+                <div class="category-title">Maison intelligente</div>
+            </div>
+            <div class="category">
+                <img src="./images/lighting-li001.avif" alt="Summer SALE">
+                <div class="category-title">Éclairage</div>
+            </div>
+            <div class="category">
+                <img src="./images/beds-mattresses-bm001.avif" alt="All furniture">
+                <div class="category-title">Lits</div>
+            </div>
+            <div class="category">
+                <img src="./images/decoration-de001.avif" alt="Summer SALE">
+                <div class="category-title">Décoration intérieure</div>
+            </div>
+            <div class="category">
+                <img src="./images/laundry-cleaning-lc001.avif" alt="New">
+                <div class="category-title">Blanchisserie & nettoyage</div>
+            </div>
+            <div class="category">
+                <img src="./images/pet-products-pt001.avif" alt="All furniture">
+                <div class="category-title">Produits pour animaux de compagnie</div>
+            </div>
+            
+        </div>
+        <div class="arrow" id="right-arrow">&gt;</div>
+    </div>
+</div>
+</main>
