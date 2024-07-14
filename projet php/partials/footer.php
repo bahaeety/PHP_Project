7f518cf7ed6d80b575d7e8ea3c6d9ca744f040e8
Abdelalim <162515953+AbdelalimB1729@users.noreply.github.com>
2024-07-14 10:56:18 -0400

    Footer

---
+++ projet php/partials/footer.php
@@ -0,0 +1,119 @@
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container-fluide m-0 p-0">
+    <!-- Footer -->
+    <footer class="text-center text-lg-start text-white bg-black" >
+        <!-- Grid container -->
+        <div class="container p-4 pb-0">
+            <!-- Section: Links -->
+            <section class="">
+                <!--Grid row-->
+                <div class="row">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">
+                            Harmony Home
+                        </h6>
+                        <p>
+                            Bienvenue chez Harmony Home, votre destination pour des meubles et 
+                            accessoires de maison élégants et abordables. Transformez votre espace 
+                            en un havre de paix et de style avec nos produits .
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+                        <p>
+                            <a class="text-white"  href="Maison intelligente.php">Maison intelligente</a>
+                        </p>
+                        <p>
+                            <a class="text-white" href="Lits.php">Lits</a>
+                        </p>
+                        <p>
+                            <a class="text-white" href="Décoration intérieure.php">Décoration intérieure</a>
+                        </p>
+                        <p>
+                            <a class="text-white" href="Éclairage.php">Éclairage</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">
+                            Useful links
+                        </h6>
+                        <p>
+                            <a class="text-white">Your Account</a>
+                        </p>
+                        <p>
+                            <a class="text-white">Gift cards</a>
+                        </p>
+                        <p>
+                            <a class="text-white">Shipping </a>
+                        </p>
+                        <p>
+                            <a class="text-white">About Us</a>
+                        </p>
+                    </div>
+
+                    <!-- Grid column -->
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                        <p><i class="fas fa-home mr-3"></i> Montreal, Quebec, Canada</p>
+                        <p><i class="fas fa-envelope mr-3"></i> Harmony.Home@gmail.com</p>
+                        <p><i class="fas fa-phone mr-3"></i>1 855 588 2436</p>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
+
+            <hr class="my-3">
+
+            <!-- Section: Copyright -->
+            <section class="p-3 pt-0">
+                <div class="row d-flex align-items-center">
+                    <!-- Grid column -->
+                    <div class="col-md-7 col-lg-8 text-center text-md-start">
+                        <!-- Copyright -->
+                        <div class="p-3">
+                            © Harmony Home - Inspiring Comfort & Style, One Home at a Time.
+                        </div>
+                        <!-- Copyright -->
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+                        <!-- Facebook -->
+                        <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-facebook-f"></i></a>
+
+                        <!-- Twitter -->
+                        <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-twitter"></i></a>
+
+                        <!-- Google -->
+                        <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-google"></i></a>
+
+                        <!-- Instagram -->
+                        <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+            </section>
+            <!-- Section: Copyright -->
+        </div>
+        <!-- Grid container -->
+    </footer>
+    <!-- Footer -->
+</div>
+<!-- End of .container -->
--- projet php/views/acceuil.php
+++ projet php/views/acceuil.php
@@ -6,6 +6,7 @@
     <title>acceuil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" defer ></script>
 <script src="../script1.js" defer></script>
@@ -15,5 +16,7 @@
     <?php require( '../partials/header.php' ) ?>
     <?php require( '../partials/main.php' ) ?>
 
+
+    <?php require( '../partials/footer.php' ) ?>
 </body>
 </html>
